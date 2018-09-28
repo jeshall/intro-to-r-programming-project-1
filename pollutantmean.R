203f1017720a2567f31d99b64f51031d2dcc02a9
Jesse Hall <jessehall10989@yahoo.com>
2018-09-28 10:26:44 -0400

    remove current directory prepended to filename

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,7 +2,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   pollutants <- c()
   
   for (current_id in id) {
-    current_frame <- read.csv(sprintf("./%s/%03d.csv", directory, current_id))
+    current_frame <- read.csv(sprintf("%s/%03d.csv", directory, current_id))
     pollutants <- append(pollutants, current_frame[pollutant])
   }
   
